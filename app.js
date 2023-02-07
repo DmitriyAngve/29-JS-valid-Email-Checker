8e730805b9208bfc4b00431ff0087fee15f4ab1c
Dmitriy Angve <kirirch129@gmail.com>
2023-02-07 11:57:39 +0300

    #

--- app.js
+++ app.js
@@ -1,10 +1,16 @@
 const btn = document.querySelector("button");
 const myInput = document.querySelector("input");
-const results = document.querySelector(".results0");
+const results = document.querySelector(".results");
 
 myInput.value = "dmitriy@gmail.com";
 const regExp1 = /([A-Za-z0-9._-]+@[A-Za-z0-9._-]+\.[A-Za-z0-9._-]+)/gi;
 
 btn.addEventListener("click", () => {
-  console.log(regExp1.test(myInput.value));
+  if (regExp1.test(myInput.value)) {
+    results.style.color = "green";
+    results.textContent = "Valid Email :)";
+  } else {
+    results.style.color = "red";
+    results.textContent = "Not an Email!";
+  }
 });
